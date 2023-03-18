1708d0b2068262a167de0aefd0381c87db3d502b
a.egorov <mymailegorov@gmail.com>
2023-03-18 21:16:11 +0300

    feat: add database seeder project

--- src/src/PetProject.IdentityServer/src/PetProject.IdentityServer.DbSeeder/Program.cs
+++ src/src/PetProject.IdentityServer/src/PetProject.IdentityServer.DbSeeder/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using PetProject.IdentityServer.Database.Extensions;
+
+namespace PetProject.IdentityServer.DbSeeder;
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        await CreateHostBuilder(configuration).RunConsoleAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(IConfiguration configuration)
+    {
+        var logger = new LoggerConfiguration()
+            .ReadFrom
+            .Configuration(configuration)
+            .CreateLogger();
+
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddIdentityDatabase(options =>
+                {
+                    options.ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+                    options.IsDevelopment = true;
+                });
+
+                services.AddHostedService<SeederHostedService>();
+            }).UseSerilog(logger);
+    }
+}
