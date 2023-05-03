5dd48d8a8e08f88b0d10d3cd15469e85e721015a
a.egorov <mymailegorov@gmail.com>
2023-05-03 20:22:59 +0300

    feat: add extension for adding product database

---
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.MongoDb/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetProject.ProductAPI.Domain.Product.Entity;
+using PetProject.ProductAPI.Domain.Interfaces.Repositories;
+using PetProject.ProductAPI.MongoDb.Contexts;
+using PetProject.ProductAPI.MongoDb.Repositories;
+
+namespace PetProject.ProductAPI.MongoDb.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddProductApiMongoDb(
+        this IServiceCollection services,
+        Action<DbContextOptions> optionsAction)
+    {
+        var _options = new DbContextOptions();
+        optionsAction?.Invoke(_options);
+
+        services.AddSingleton(_options);
+        services.AddTransient<IDbContext<Product>, ProductApiDbContext>();
+        services.AddTransient<IProductRepository, ProductRepository>();
+
+        return services;
+    }
+}
