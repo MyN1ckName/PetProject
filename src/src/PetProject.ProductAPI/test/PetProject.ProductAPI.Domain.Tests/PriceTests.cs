1ec5eae68765bebcdae754bdcd04fd5c79bb9d04
a.egorov <mymailegorov@gmail.com>
2023-05-03 21:42:05 +0300

    chore: add test for price class

---
+++ src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Domain.Tests/PriceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace PetProject.ProductAPI.Domain.Tests;
+public class PriceTests
+{
+    [Fact]
+    public void Price_more_minimum()
+    {
+        var value = 10f;
+        var price = new Price(value);
+
+        Assert.Equal(value, price.Value);
+    }
+
+    [Fact]
+    public void Price_less_minimum()
+    {
+        var value = 9.98f;
+        Assert.Throws<ArgumentException>(() => new Price(value));
+    }
+}
--- src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Domain.Tests/Usings.cs
+++ src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Domain.Tests/Usings.cs
@@ -1,1 +1,3 @@
 global using Xunit;
+global using PetProject.ProductAPI.Domain.Product.Entity;
+global using PetProject.ProductAPI.Domain.Product.ValueObject;
