db856f06493e306102743f44647338069201dc1e
a.egorov <mymailegorov@gmail.com>
2023-05-08 22:01:09 +0300

    fix: fix create category instance

--- src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Host/Controllers/JustForTestController.cs
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Host/Controllers/JustForTestController.cs
@@ -32,21 +32,21 @@ public class JustForTestController : ControllerBase
         var product1 = new Product
         {
             Name = new Name ("product 1"),
-            Category = new Category { Value = "category 1" },
+            Category = new Category("category 1"),
             Price = new Price(14.99f),
         };
 
         var product2 = new Product
         {
             Name = new Name ("product 2"),
-            Category = new Category { Value = "category 1" },
+            Category = new Category ("category 1"),
             Price = new Price(24.99),
         };
 
         var product3 = new Product
         {
             Name = new Name ("product 3"),
-            Category = new Category { Value = "category 2" },
+            Category = new Category ("category 2"),
             Price = new Price(99.99f),
         };
 
