c40791df67c835405d736f33607a1b9799d2560c
a.egorov <mymailegorov@gmail.com>
2023-05-08 22:00:22 +0300

    fix: add conditions for category

--- src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Domain/Product/ValueObject/Category.cs
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Domain/Product/ValueObject/Category.cs
@@ -2,5 +2,25 @@
 
 public class Category
 {
-    public string Value { get; init; }
+    public const int MAX_CATEGORY_LENGHT = 50;
+    private string _value;
+
+    public Category(string name)
+    {
+        Value = name;
+    }
+
+    public string Value
+    {
+        get => _value;
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("the category name must not be empty");
+            else if (value.Length > MAX_CATEGORY_LENGHT)
+                throw new ArgumentNullException($"the category name is longer than {MAX_CATEGORY_LENGHT} characters");
+            else
+                _value = value;
+        }
+    }
 }
