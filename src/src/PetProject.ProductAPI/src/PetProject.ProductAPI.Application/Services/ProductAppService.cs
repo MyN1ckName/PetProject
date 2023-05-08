3d3f2d70adbab51c5bb92b9f8dfc42377429708e
a.egorov <mymailegorov@gmail.com>
2023-05-08 22:51:37 +0300

    feat: add product app service

---
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Application/Services/ProductAppService.cs
@@ -0,0 +1,12 @@
+﻿using PetProject.ProductAPI.Application.Contracts.Dto.Product;
+using PetProject.ProductAPI.Application.Contracts.Interfaces;
+
+namespace PetProject.ProductAPI.Application.Services;
+
+public class ProductAppService : IProductService
+{
+    public Task<List<ProductDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
