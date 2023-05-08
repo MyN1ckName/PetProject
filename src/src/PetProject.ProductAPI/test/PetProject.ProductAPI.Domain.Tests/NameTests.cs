0ec7f0fe71c9fca494bc9f5e7dfe03e71649c383
a.egorov <mymailegorov@gmail.com>
2023-05-08 21:50:51 +0300

    chore: add tests fore name

---
+++ src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Domain.Tests/NameTests.cs
@@ -0,0 +1,29 @@
+﻿namespace PetProject.ProductAPI.Domain.Tests;
+
+public class NameTests
+{
+    [InlineData(null)]
+    [InlineData("")]
+    [Theory]
+    public void Name_not_null_or_empty(string name)
+    {
+        Action action = () => new Name(name);
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void Name_max_lenght_is_valid()
+    {
+        var name = new string('a', Name.MAX_NAME_LENGTH);
+        var sut = new Name(name);
+        sut.Value.Should().Be(name);
+    }
+
+    [Fact]
+    public void Name_more_max_lenght_is_invalid()
+    {
+        var name = new string('a', Name.MAX_NAME_LENGTH + 1);
+        var sut = new Name(name);
+        sut.Value.Should().Be(name);
+    }
+}
