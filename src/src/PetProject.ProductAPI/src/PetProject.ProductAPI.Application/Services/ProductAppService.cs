65d4c71430cba180d8823e60495b5db1f60bdce6
a.egorov <mymailegorov@gmail.com>
2023-05-08 23:22:17 +0300

    feat: impliment IProductAppService

--- src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Application/Services/ProductAppService.cs
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Application/Services/ProductAppService.cs
@@ -1,12 +1,30 @@
-﻿using PetProject.ProductAPI.Application.Contracts.Dto.Product;
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using PetProject.ProductAPI.Application.Contracts.Dto.Product;
 using PetProject.ProductAPI.Application.Contracts.Interfaces;
+using PetProject.ProductAPI.Domain.Product.Entity;
+using PetProject.ProductAPI.Domain.Interfaces.Repositories;
 
 namespace PetProject.ProductAPI.Application.Services;
 
 public class ProductAppService : IProductAppService
 {
-    public Task<List<ProductDto>> GetAllAsync()
+    private readonly ILogger _logger;
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+    public ProductAppService(
+        ILogger<ProductAppService> logger,
+        IProductRepository productRepository,
+        IMapper mapper)
     {
-        throw new NotImplementedException();
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task<List<ProductDto>> GetAllAsync()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return _mapper.Map<List<Product>, List<ProductDto>>(products);
     }
 }
