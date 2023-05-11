d7bda687f465d6abef4d0a09f0427391894bb75e
a.egorov <mymailegorov@gmail.com>
2023-05-11 16:15:32 +0300

    fix: delete one active device concept. later will be done

--- src/src/PetProject.IdentityServer/src/PetProject.IdentityServer.Domain/OneDeviceLocks/OneDeviceLock.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PetProject.IdentityServer.Domain.OneDeviceLocks;
-public class OneDeviceLock
-{
-    public Guid Id { get; set; }
-    public string ClientId { get; set; }
-    public Guid Hash { get; set; }
-}
