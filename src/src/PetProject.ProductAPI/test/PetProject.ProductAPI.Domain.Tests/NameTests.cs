7557a536d05bfa9e71e3f708ad69a40d1eb927bb
a.egorov <mymailegorov@gmail.com>
2023-05-14 16:08:19 +0300

    fix: fix name lenght tets

--- src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Domain.Tests/NameTests.cs
+++ src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Domain.Tests/NameTests.cs
@@ -2,6 +2,8 @@
 
 public class NameTests
 {
+    private const int MAX_NAME_LENGTH = 50;
+
     [InlineData(null)]
     [InlineData("")]
     [Theory]
@@ -14,7 +16,7 @@ public class NameTests
     [Fact]
     public void Name_max_lenght_is_valid()
     {
-        var name = new string('a', Name.MAX_NAME_LENGTH);
+        var name = new string('a', MAX_NAME_LENGTH);
         var sut = new Name(name);
         sut.Value.Should().Be(name);
     }
@@ -22,7 +24,7 @@ public class NameTests
     [Fact]
     public void Name_more_max_lenght_is_invalid()
     {
-        var name = new string('a', Name.MAX_NAME_LENGTH + 1);
+        var name = new string('a', MAX_NAME_LENGTH + 1);
         var action = () => new Name(name);
         action.Should().Throw<ArgumentException>();
     }
