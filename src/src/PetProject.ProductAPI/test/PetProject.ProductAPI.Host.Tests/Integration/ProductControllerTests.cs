e8bde9eb676fa8c8d36ac37da50ae8de0761a520
a.egorov <mymailegorov@gmail.com>
2023-05-17 20:38:35 +0300

    chore: add tests for update product method

--- src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Host.Tests/Integration/ProductControllerTests.cs
+++ src/src/PetProject.ProductAPI/test/PetProject.ProductAPI.Host.Tests/Integration/ProductControllerTests.cs
@@ -34,7 +34,7 @@ public class ProductControllerTests : IntegrationTest
     }
 
     [Fact]
-    public async void Insert_product_should_has_non_default_id()
+    public async Task Insert_product_should_has_non_default_id()
     {
         await ClearDatabaseAsync();
 
@@ -52,6 +52,39 @@ public class ProductControllerTests : IntegrationTest
         result.StatusCode.Should().Be(201);
         var value = result.Value.Should().BeOfType<EntityDto<Guid>>().Subject;
         value.Id.Should().NotBe(default(Guid));
+    }
+
+    [Fact]
+    public async Task Update_product_should_return_204_and_update_properties()
+    {
+        await ClearDatabaseAsync();
+
+        var insertProduct = new CreateProductDto
+        {
+            Name = "product test name",
+            Category = "test category",
+            Price = 99.99f,
+        };
+        var token = new CancellationTokenSource().Token;
+        var id = (((await _controller.InsertOneAsync(insertProduct, token)) as ObjectResult)!.Value as EntityDto<Guid>)!.Id;
+
+        var productUpdate = new ProductDto
+        {
+            Id = id,
+            Name = "new product test name",
+            Category = "new test category",
+            Price = 999.99f
+        };
+
+        var sut = await _controller.UpdateOneAsync(productUpdate, token);
+        var result = sut.Should().BeOfType<StatusCodeResult>().Subject;
+        result.StatusCode.Should().Be(204);
+
+        var product = ((await _controller.GetAsync(id, token)) as ObjectResult)!.Value as ProductDto;
+
+        product!.Name.Should().Be(productUpdate.Name);
+        product!.Category.Should().Be(productUpdate.Category);
+        Math.Round(product!.Price, 2).Should().Be(Math.Round(productUpdate.Price, 2));
     }
 
     private async Task ClearDatabaseAsync() =>
