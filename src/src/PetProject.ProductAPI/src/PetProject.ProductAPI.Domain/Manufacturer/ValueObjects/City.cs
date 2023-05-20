51b7bcf379dab4d77df8b0be9261df2b5c1de5ae
Aleksandr Egorov <mymailegorov@gmail.com>
2023-05-20 19:01:54 +0300

    feat: add manufacturer entity

---
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Domain/Manufacturer/ValueObjects/City.cs
@@ -0,0 +1,11 @@
+﻿namespace PetProject.ProductAPI.Domain.Manufacturer.ValueObjects;
+
+public class City : ValueObject
+{
+    public string Value { get; set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value; 
+    }
+}
