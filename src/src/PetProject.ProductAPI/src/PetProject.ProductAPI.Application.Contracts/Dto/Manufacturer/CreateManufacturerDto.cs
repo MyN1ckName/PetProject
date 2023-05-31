c7208362de7ad2049ae450f33fce03943554c642
Aleksandr Egorov <mymailegorov@gmail.com>
2023-05-31 16:36:34 +0300

    feat: create manufacturer method in controller

---
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Application.Contracts/Dto/Manufacturer/CreateManufacturerDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PetProject.ProductAPI.Application.Contracts.Dto.Manufacturer;
+
+public class CreateManufacturerDto
+{
+    public string Name{ get; set; }
+    public string City{ get; set; }
+}
---
+++ src/src/PetProject.ProductAPI/src/PetProject.ProductAPI.Host/Controllers/ManufacturerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PetProject.ProductAPI.Domain.Manufacturer.Entitys;
+using PetProject.ProductAPI.Domain.Interfaces.Repositories;
+using PetProject.ProductAPI.Application.Contracts.Dto.Manufacturer;
+
+namespace PetProject.ProductAPI.Host.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class ManufacturerController : ControllerBase
+{
+    private readonly ILogger _logger;
+    private readonly IManufacturerRepository _repository;
+
+    public ManufacturerController(
+        ILogger<ManufacturerController> logger,
+        IManufacturerRepository repository)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateManufacturerDto input)
+    {
+        var manufaturer = Manufacturer.Create(input.Name, input.City);
+        await _repository.InsertOneAsync(manufaturer);
+
+        // return Created();
+        return new StatusCodeResult(201);
+    }
+}
